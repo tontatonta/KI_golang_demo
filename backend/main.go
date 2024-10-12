1b72a7908866780c2dda6c29a14071100e6a364b
Inomata <tonyhawkingsexample@example.com>
2024-10-12 17:52:12 +0900

    moduleできない

--- backend/main.go
+++ backend/main.go
@@ -11,6 +11,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", handler)
-    fmt.Println("Starting server on :8080")
-    http.ListenAndServe(":8080", nil)
+    fmt.Println("Starting server on :8000")
+    http.ListenAndServe(":8000", nil)
 }
